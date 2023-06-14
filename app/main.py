562fe67eb8b1db9d5551799a3d63d0788374c3fd
Pedro Mendes <pmo2002@gmail.com>
2023-06-14 16:56:34 -0300

    fixing access-control-allow-origins error

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 
-origins = ["http://localhost:8080", "https://revisapp.vercel.app"]
+origins = ["https://revisapp.vercel.app"]
 
 
 middleware = [
